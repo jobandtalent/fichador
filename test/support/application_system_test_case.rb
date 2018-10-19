b4f45dd6e348ea81ce8dbd9483eaf4e9ba923788
GermanDZ <germ@ndz.com.ar>
2018-10-19 16:39:42 +0200

    config tests to use custom selenium settings

---
+++ test/support/application_system_test_case.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+require 'socket'
+
+class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  class << self; attr_accessor :host end
+
+  def self.resolve_host_address
+    IPSocket.getaddress(Socket.gethostname)
+  rescue StandardError
+    puts 'Cannot identify host address for Capybara, using 127.0.0.1'
+    '127.0.0.1'
+  end
+
+  def self.config
+    Capybara.app = Rack::Builder.new_from_string File.read(Rails.root.join('config.ru'))
+
+    @host = ENV.fetch('CAPYBARA_HOST') { ApplicationSystemTestCase.resolve_host_address }
+  end
+
+  def self.driver_options(options = {})
+    ENV['SELENIUM_SERVER_URL'].tap { |value| options[:url] = value unless value.nil? }
+    options
+  end
+
+  puts "Using selenium: #{driver_options.inspect}"
+  driven_by :selenium,
+            using: :chrome,
+            screen_size: [1280, 800],
+            options: driver_options
+
+  def setup
+    Capybara.server_host = ApplicationSystemTestCase.host
+    host! "http://#{Capybara.server_host}"
+    super
+  end
+end
